7300d5d6782b90ef0ad605c623dd8aeba3261b84
atlmendoza <atlmendoza@pshs.edu.ph>
2026-01-21 06:53:27 +0800

    changed to YMSAT 2026

--- links.js
+++ links.js
@@ -1,108 +1,71 @@
 //names of the image files
-var links = new Array();
-links[0]={section:"9Cs",title:"ArtWalli",desc:"Our website, ArtWalli, is an interactive website where users can either draw, design, or download their very own wallpapers.",url:"https://pih-rah-tees.github.io/wdprojcesiumpanistetangco/public",icon:"cs2328.png",by:"panis&tetangco"}
-links[1]={section:"9Cs",title:"CipherMania",desc:"Learn more about our inspirations behind this website and the concept of ciphers too! With these, inspire an interest in encrypted and decrypted communication!",url:"https://sarbuyco.github.io/Ciphermania/public",icon:"cs0711.png",by:"buyco&cruz"}
-links[2]={section:"9Cs",title:"Mindless Motivation",desc:"Mindless Motivation is a mental health website where struggling students can open as a source for emotions.",url:"https://typical-pentagonal-vessel.glitch.me/",icon:"mindless.png",by:"Arcilla&esguerra"}
-links[3]={section:"9Cs",title:"Aklatan-Mo: Aklatan-Ko",desc:"Your gateway to a world of literature. Start reading our collection of top-rated books across various genres, from classics to bestsellers.",url:"https://geroge-real.github.io/AMAKcs/public/",icon:"aklatan.png",by:"froyalde&adriano"}
-links[4]={section:"9Cs",title:"Cherry On Top",desc:"Cherry on Top is an interactive game where users can decorate and display their own sweet treat.",url:"https://cs3-proj-cesium-luz-triguero.glitch.me/homepage.html",icon:"cherryontop.jpg",by:"luz&triguero"}
-links[5]={section:"9Cs",title:"FitFormula",desc:"FitFormula,  is designed to help users start and go through their excercise and fitness experience by providing calculation tools,  plans according to the user's fitness situation, and other pages.",url:"https://elijahandrei.github.io/Q3DimayugaMasangkayProject/public/index.html",icon:"cs1219.png",by:"dimayuga&msangkay"}
-links[6]={section:"9Cs",title:"Music from Mars",desc:"All about Bruno Mars and his music", url:"https://ralffytulfo.github.io/wdprojsectionsanjoseandmendozav1/public/index.html",icon:"cs2127.png",by:"sanjose&mendoza"}
-links[7]={section:"9Cs",title:"Solidarity Network",desc:"Our website effectively aids the extension of monetary donations to the innocent victims of war, by compiling all their respective donation links into one webpage, allowing for a more convenient transaction.", url:"https://aypltra.github.io/solidarity-nework/public/",icon:"cs0322.png",by:"alcantara&moritcho"}
-links[8]={section:"9Sr",title:"SPARC",
-	desc:"SPARChives is the online platform of SPARC used to store and access reviewers, mock tests, and other resources of SPARC.", 
-	url:"https://mathematicalcoder.github.io/sparchives/public/",icon:"sr-kaiser-sparc.png",by:"Kaiser&Schiel"}
-links[9]={section:"9Sr",title:"Body Blueprint",
-		desc:"The purpose of this website is to map the body and navigate body parts to find out the most efficient way to train them by setting out a body map and by clicking one body part and show the most efficient ways to train them.", 
-		url:"sr-rahon-body_blueprint/",icon:"sr-rahon-body.png",by:"Rahon&Leona"}
-links[10]={section:"9Li",title:"Calm Quest",
-	desc:" CalmQuest is a website that aims to allow users to understand and improve through various available resources.", 
-	url:"li-lucas_johann-calm_quest/",icon:"li-lucas_johann-calm_quest.png",by:"Lucas&Johann"}
-links[11]={section:"9Li",title:"Modern Family",
-	desc:"With its witty storytelling and lovable characters, Modern Family became a critically acclaimed series that resonated with audiences worldwide", 
-	url:"li-rio_zach-modern_fam/",icon:"li-rio_zach-modern_fam.png",by:"Rio&Zach"}
-links[12]={section:"9Na",title:"Sinag ng Kalayaan",
-	desc:"Sinag ng Kalayaan: The Next Step is a thrilling interactive game that brings life the dramatic choices and consequences of the Spanish Colonial Era. ", 
-	url:"na-cara_rheena-sinag_kalayaan/",icon:"na-cara_rheena-sinag_kalayaan.png",by:"Cara&Rheena"}
-links[13]={section:"9Na",title:"Kanlungan",
-	desc:"kanlungan is a safe space 'shelter' dedicated for people who might be lost in their pathway of life. adorned in charming digital artwork and led by the fictional shapeshifting cat, Miskonasia, the website holds several little ephemeras in the daily — helpful quotes for the day with an originally written poem in the side, a post-it note station as fleeting as you allow it to be, and a bit of an 'achievements' section dictated by the amount of days you log in. ", 
-	url:"na-esquivel_villasfer-kanlungan/",icon:"na-esquivel_villasfer-kanlungan.png",by:"Elisha&Joshua"}
-links[14]={section:"9K",title:"The Ciphermind",
-		desc:"PuzzledYou is our website about our own developed personality types with a theme of puzzles and word games.", 
-		url:"https://mads-avant.github.io/avante-balingit-potassium-project-draft/public/",icon:"k0204.png",by:"avante&balingit"}
-links[15]={section:"9K",title:"Featured Sets",
-    desc:"Our website, made by Enzo Edles and Fred Calitis helps people expand their imagination through the connection of bricks of different shapes and sizes to recreate structures or let their creativity go wild.", 
-    url:"https://wdprojpotassiumcalitis-edles.glitch.me",icon:"k0511.png",by:"edles&calitis"}
-links[16]={section:"9K",title:"CampusCalc",
-    desc:"CampusCalc is an all-in-one platform that helps students effortlessly calculate their GWA/GPA, matchmake universities, and explore universities through its matchmaking and discovery tools in a user-friendly interface", 
-    url:"https://wdprojpotassiumvibalignacio.glitch.me/",icon:"k1730.png",by:"vibals&ignacio"}
-links[17]={section:"9K",title:"QuizQuest",
-    desc:"QuizQuest is an interactive trivia game where players can test their knowledge across various categories like history, science, and pop culture full of engaging questions and included with a timer, users can compete for the highest score while tracking their progress on the leaderboard.", 
-    url:"https://lazaro-csproject.glitch.me/index.html",icon:"k19.png",by:"lazaro"}
-links[18]={section:"9K",title:"",
-    desc:"Meya’s Vogue is a nostalgic Y2K fashion inspired website where you can complete tasks, dress up, and look through our gallery with pre-made outfits for inspiration", 
-    url:"https://finalproj-calo-luquias.glitch.me",icon:"k0621.png",by:"calo&luquias"}
-links[19]={section:"9Rb",title:"Dress the Best",
-    desc:"Dress the Best! Create your own characters and make them say anything!", 
-    url:"https://wdprojrubidiumhutallapuga.glitch.me/index.html",icon:"hutalla.png",by:"hutalla"}
-links[20]={section:"9Rb",title:"WindBreaker",
-    desc:"WindBreaker.fandom is a fan made website engineered to bring the readers of the Wind Breaker webtoon closer to its world. ", 
-    url:"https://wdprojrubidiummaniagorequioma.glitch.me/index.html",icon:"rb1824.png",by:"liz&raj"}
-links[21]={section:"9Rb",title:"Mystwood Mysteries",
-    desc:"A mysterious game where players get to step into the shoes of a detective to solve crime cases where they will embark on three challenges levels (Easy, Medium, and Hard) and each one will make them unravel the hidden secrets and piece together cryptic clues.", 
-    url:"https://mikanono21.github.io/wdprojrubidiumnonosantos/public/",icon:"rb2127.png",by:"santos&nono"}
-links[22]={section:"9Rb",title:"CATACLYSIM",
-    desc:"Discover the dangers of disasters in the Philippines and the world.", 
-    url:"https://wdprojrubidiumcanlassigua.glitch.me/index.html",icon:"rb2805.png",by:"canlas&sigua"}
-links[23]={section:"9Rb",title:"Innerself Explorer",
-    desc:"InnerSelf Explorer is an interactive platform offering engaging personality quizzes that provide fun, personalized insights with dynamic feedback and shareable results.", 
-    url:"https://9rb-aguilar-pascual-projprop.glitch.me/index.html",icon:"rb0122.png",by:"aguilar&pascual"}
-links[24]={section:"9Na",title:"Alter the World",
-	desc:"Welcome to To Alter the World! A comprehensive website dedicated to exploring the world of geometric transformations in math, including translation, rotation, and dilation.", 
-	url:"na-matt_lewis-translation/",icon:"na-matt_lewis-translation.png",by:"Matt&Lewis"}
-links[25]={section:"9Mg",title:"Click Civilization",
-	desc:"This website contains fun minigames and applications all about clicking!", 
-	url:"https://rajveermadrid.github.io/WDProjMagnesiumLopingcoMadrid/public/index.html", 
-	icon:"mg-rajveer_cait-click_civ.png",by:"Rajveer&Cait"}	
-links[26]={section:"9Mg",title:"Heartspire",
-	desc:"At Heartspire Foundation, we believe in the extraordinary power of empathy to transform lives and build a better world.", 
-	url:"https://christiansolidum.github.io/project-proposal-cs3--solidum-sulicipan/", 
-	icon:"mg-christian_von-heartspire.png",by:"Christian&Von"}	
-links[27]={section:"9Be",title:"Stick Chase",
-	desc:"A game to erase the stickman", 
-	url:"https://wdprojberylliumaducalaguda.glitch.me/index.html", 
-	icon:"aducal.png",by:"Aducal"}	
-links[28]={section:"9Be",title:"DubiDubiTapTap",
-	desc:"DubiDubiTapTap is a website that contains simple, fun, and accessible offline games. “Dubi” is short for “digital buddy", 
-	url:"https://wdprojberylliumbaguioreblorasalvador.glitch.me/", 
-	icon:"iesu.png",by:"baguio&reblora&salvador"}	
-links[29]={section:"9Be",title:"Chessmate",
-	desc:"Chessmate is a website that allows users to access chess-related content and play interactive chess games.", 
-	url:"https://wdprojberylliumcruzsangalang-.glitch.me/index.html", 
-	icon:"be1327.png",by:"cruz&sangalang"}	
-links[30]={section:"9Sr",title:"Battleship",
-	desc:"Welcome to Battleship! A classic game of strategy and luck, where players will try to sink each other's ships before their own ships are sunk.", 
-	url:"sr-don_rainer-battleship/", 
-	icon:"sr-don_rainer-battleship.png",by:"Don&Rainer"}	
-links[31]={section:"9Sr",title:"Play Chess",
-	desc:"A website where you can play and know the history and objectives about Chess.", 
-	url:"sr-sony_panis-chess/", 
-	icon:"sr-sony_panis-chess.png",by:"Sony&Panis"}	
-links[32]={section:"9Sr",title:"Miraculous Ladybug",
-	desc:"A collection of personal opinions, images, synopses, and more about Miraculous Ladybug.", 
-	url:"https://hasherbrownie.github.io/wdprojstrontiumganpadilla/public/index.html", 
-	icon:"sr-jamie_maize-miraculous.png",by:"Jamie&Maize"}	
-links[33]={section:"9Li",title:"Digipop",
-	desc:"DigiPop is the ultimate digital playground where collectors can challenge themselves through fun mini-games to customize and unlock unique figurines like Smiskis and Mofusands, all designed by Lucille Marie Dalope and Francesca Carmen Bagnes of 9 - Lithium!", 
-	url:"li-lucille-digipop/", 
-	icon:"li-lucille-digipop.png",by:"Lucille&Cheska"}	
-links[34]={section:"9Na",title:"Mobile Legends", 
-	desc:"The website provides an insightful and comprehensive guide to Mobile Legends players as this will cover the best heroes, builds, and effective combos to help you climb the ranks.", 
-	url:"na-seguin_tamayo-ml/", 
-	icon:"na-seguin_tamayo-ml.png",by:"Mirkki&Josh"}	
-links[35]={section:"9Na",title:"Spill the Deej", 
-	desc:"A fun place to hang and send secret messages but could lead to more if you look deeper.", 
-	url:"na-raph_juls-deej/", 
-	icon:"na-raph_juls-deej.png",by:"Raph&Juls"}	
+var links = [];
+/* template
+links[1]={
+	section:"",
+	title:"",
+	desc:"",
+	url:"",
+	icon:"",
+	by:"",
+	stat:  // to indicate acceptance - true (for displaying) or false (means conditional after some edits) 
+}
+*/
+links[0]={
+	section:"9Be",
+	title:"Shakespeare's Quill",
+	desc:"This is Shakespeare's Quill, a visually rich exploration of William Shakespeare’s world, blending dark design, animation, and literary history into a timeless experience.",
+	url:"https://youngerpanao02.github.io/WDProjBerylliumPanaoTamon/",
+	icon:"be2328.png",
+	by:"PanaoTamon",
+	stat: false
+}
+links[1]={
+	section:"9Be",
+	title:"Pamana Adventures",
+	desc:"Pamana Adventures is an interactive travel website where users create a personalized profile, explore destinations tailored to their interests and budget, and discover curated meaningful adventures across the Philippines made just for them.",
+	url:"https://rafaelayatco.github.io/WDProjBeGaborniYatco/",
+	icon:"",
+	by:"",
+	stat:false
+}
+links[2]={
+	section:"9be",
+	title:"Icons of Ghibli",
+	desc:"Whispers of Ghibli is a very emotional and sincere trip to the dream-like realm of the Studio Ghibli—where art, imagination, and feelings come together to create storytelling that will never be forgotten.",
+	url:"https://aravilalado.github.io/WDProjBeMacatangayVillados/public/homepage.html",
+	icon:"b1929.png",
+	by:"",
+	stat:false
+}
+links[3]={
+	section:"9Cs",
+	title:"MineMaster",
+	desc:"Minemaster is a collection of different guides designed to give inspiration to and improve your builds in the game Minecraft.",
+	url:"https://frncjcnt.github.io/WDProjCesiumJacintoMapalo/index.html",
+	icon:"Cs1419.png"
+	by:"Mateo&Mapalo",
+	stat:false
+}
+links[4]={
+	section:"9Cs",
+	title:"Pizza Papi",
+	desc:"Inspired by casual cooking games, this interactive website celebrates pizza through a game with rankings, historical insights, and creator details to provide an engaging experience for everyone from hungry audiences to pizza enthusiasts.",
+	url:"https://steph0801.github.io/WDProjCesiumBasconMargarito/",
+	icon:"cs0520.png",
+	by:"Bascon&Margarita",
+	stat:true
+}
+links[5]={
+	section:"",
+	title:"",
+	desc:"",
+	url:"",
+	icon:"",
+	by:"",
+	stat:
+}
+
 		
 	
 function dispProj(){
@@ -125,13 +88,15 @@ function dispProj(){
   // construct content of the body
 	var tobeDisp = ""
 	for (let i=0;i<links.length;i++) {
-	    tobeDisp +=	 
-			'<div class="gallery">'+
-			'<a target="_blank" href="' + links[genNum[i]].url +'">'+
-			'<div> <img src="img\\'+links[genNum[i]].icon +'" /></div>'+
-		   	'<div class="desc"><span>'+links[genNum[i]].title+'</span>'+
-                        '<p>'+links[genNum[i]].desc+'</p></div>'+
-			'</a></div>'
+		if (links[i] || links[i].stat ) { 
+			tobeDisp +=	 
+				'<div class="gallery">'+
+				'<a target="_blank" href="' + links[genNum[i]].url +'">'+
+				'<div> <img src="img\\'+links[genNum[i]].icon +'" /></div>'+
+				'<div class="desc"><span>'+links[genNum[i]].title+'</span>'+
+							'<p>'+links[genNum[i]].desc+'</p></div>'+
+				'</a></div>'
+		}
 	}
 		
 document.getElementById("pHolder").innerHTML = tobeDisp;
